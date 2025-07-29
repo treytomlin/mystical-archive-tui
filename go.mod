ea09137559980e995df7b8c1e4a2391d31cb64fd
Trey Tomlin <treytomlin91@gmail.com>
2025-07-29 13:11:56 -0400

    adds go.mod, updates main.go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/treytomlin/mystical-archive-tui
+
+go 1.23.2
+
+require (
+    github.com/charmbracelet/bubbletea v1.3.6
+)
+
--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
-// These imports will be used later on the tutorial. If you save the file
-// now, Go might complain they are unused, but that's fine.
-// You may also need to run `go mod tidy` to download bubbletea and its
-// dependencies.
-// import (
-//     "fmt"
-//     "os"
+import (
+	"fmt"
+	"os"
 
-//     tea "github.com/charmbracelet/bubbletea"
-// )
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 type model struct {
 	choices  []string         // items on the to-do list
@@ -74,4 +70,42 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	// Return the updated model to the Bubble Tea runtime for processing.
 	// Note that we're not returning a command.
 	return m, nil
+}
+
+func (m model) View() string {
+	// The header
+	s := "What should we buy at the market?\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
